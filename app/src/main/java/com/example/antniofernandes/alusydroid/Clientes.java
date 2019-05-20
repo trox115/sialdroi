5e7ba343b3a5b82345f5bbad4c34b8ee3493447a
trox115 <tofernandes67@gmail.com>
2019-05-20 11:58:19 +0100

    Adionar clientes novo painel

--- app/src/main/java/com/example/antniofernandes/alusydroid/Clientes.java
+++ app/src/main/java/com/example/antniofernandes/alusydroid/Clientes.java
@@ -1,13 +1,39 @@
 package com.example.antniofernandes.alusydroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Clientes extends AppCompatActivity {
+public class Clientes extends AppCompatActivity implements janelanovo.ExampleDialogListener2 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clientes);
+
+        Button clientes= (Button) findViewById(R.id.button6);
+
+        clientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog7();
+            }
+        });
+    }
+
+
+    public void openDialog7(){
+        janelanovo exemplo =new janelanovo();
+        exemplo.show(getSupportFragmentManager(),"Janela de Exemplo");
+    }
+
+    public void applyTexts5(String s, String s2, String s3, String s4) {
+
+
+
+        Toast.makeText(Clientes.this, s4, Toast.LENGTH_LONG).show();
     }
 }
